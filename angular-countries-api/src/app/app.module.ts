1d6fe2af2f7b5d3615d5a0f24f9d067d4194a5d8
Giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2023-09-22 08:17:03 +0400

    filter added

--- angular-countries-api/src/app/app.module.ts
+++ angular-countries-api/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { TitleBarComponent } from './features/title/components/title-bar/title-bar.component';
 import { LayoutComponent } from './core/components/layout/layout.component';
 import { SearchBarComponent } from './features/search/components/search-bar/search-bar.component';
+import { FilterBarComponent } from './features/filter/components/filter-bar/filter-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, TitleBarComponent, LayoutComponent, SearchBarComponent],
+  declarations: [AppComponent, TitleBarComponent, LayoutComponent, SearchBarComponent, FilterBarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
